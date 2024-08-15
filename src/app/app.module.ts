d8c0f887d5ceec0d4e067cff77885d9585d145cc
Miguel-Portella <miguelportella2008@gmail.com>
2024-08-15 16:34:28 -0400

    Componente dentro de componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { CardButtonComponent } from './card/card-button/card-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    CardButtonComponent
   ],
   imports: [
     BrowserModule,
